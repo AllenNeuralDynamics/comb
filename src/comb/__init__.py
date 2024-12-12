605b083f058c0cd75cc905e8851a62c889bf10f0
mattjdavis <matt.davis@alleninstitute.org>
2024-12-12 21:54:54 +0000

    fix: import and plane order back to default

--- src/comb/__init__.py
+++ src/comb/__init__.py
@@ -1,3 +1,14 @@
 """Simple package to demo project structure.
 """
 __version__ = "0.1.6"
+
+from .behavior_ophys_dataset import BehaviorOphysDataset, BehaviorMultiplaneOphysDataset
+from .ophys_plane_dataset import OphysPlaneDataset
+from .behavior_session_dataset import BehaviorSessionDataset
+
+__all__ = [
+    "BehaviorOphysDataset",
+    "BehaviorMultiplaneOphysDataset",
+    "BehaviorSessionDataset",
+    "OphysPlaneDataset"
+]
