713f9df406539bb96dfdd94e4eda869c7a6b9f61
Iryna Yavorksa <iryna.yavorska@alleninstitute.org>
2025-04-18 22:23:47 +0000

    Added Face and Nose keys, updated behavior and eye keys to match data_file_keys.py

--- src/comb/processing/sync/sync_dataset.py
+++ src/comb/processing/sync/sync_dataset.py
@@ -90,19 +90,33 @@ class SyncDataset(object):
     PHOTODIODE_KEYS = ('photodiode', 'stim_photodiode')
     OPTOGENETIC_STIMULATION_KEYS = ("LED_sync", "opto_trial")
     EYE_TRACKING_KEYS = ("eye_frame_received",  # Expected eye tracking
-                                                # line label after 3/27/2020
-                         # clocks eye tracking frame pulses (port 0, line 9)
-                         "cam2_exposure",
-                         # previous line label for eye tracking
-                         # (prior to ~ Oct. 2018)
-                         "eyetracking",
-                         "eye_cam_exposing",
-                         "eye_tracking")  # An undocumented, but possible eye tracking line label  # NOQA E114
+                                            # line label after 3/27/2020
+                        # clocks eye tracking frame pulses (port 0, line 9)
+                        "cam2_exposure",
+                        # previous line label for eye tracking
+                        # (prior to ~ Oct. 2018)
+                        "eyetracking",
+                        "eye_tracking",# An undocumented, but possible eye tracking line label  # NOQA E114
+                        "eye_cam_frame_readout", #4/18/2025
+                        "eye_cam_exposing", #4/18/2025
+                        )  
     BEHAVIOR_TRACKING_KEYS = ("beh_frame_received",  # Expected behavior line label after 3/27/2020  # NOQA E127
-                                                    # clocks behavior tracking frame # NOQA E127
-                                                    # pulses (port 0, line 8)
-                              "cam1_exposure",
-                              "behavior_monitoring")
+                                                 # clocks behavior tracking frame # NOQA E127
+                                                 # pulses (port 0, line 8)
+                            "cam1_exposure",
+                            "behavior_monitoring",
+                            "beh_cam_frame_readout", #4/18/2025
+                            "beh_cam_exposing", # 4/18/2025
+                            ) 
+
+    # Below line labels were added on 4/18/2025, based on sync file content                           
+    FACE_TRACKING_KEYS = ("face_cam_frame_readout",
+                          "face_cam_exposing",
+                          )
+
+    NOSE_TRACKING_KEYS = ("nose_cam_frame_readout",
+                          "nose_cam_exposing",
+                          )
 
     DEPRECATED_KEYS = ("cam2_exposure", "eye_frame_received", "eyetracking", "cam1_exposure")
 
